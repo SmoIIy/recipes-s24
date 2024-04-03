1ffd29491205dc1cbc9a2d66f36937808e8cca18
Jonas Holbech <jh@jonasholbech.dk>
2024-04-03 10:23:13 +0200

    post

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ import {
 import { setupCountries } from "./modules/setup.js";
 
 setupCountries();
-
+/*
+Vortes form
+event submit
+samle data FormData()
+    .get()
+    .getAll()
+*/
 async function showRecipes() {
   const response = await getRecipes();
+  console.table(response);
   const el = document.querySelector("template").content;
   const parent = document.querySelector(".recipes");
   parent.innerHTML = "";
@@ -34,10 +41,34 @@ async function showRecipes() {
     clone
       .querySelector("button[data-action='update']")
       .addEventListener("click", async () => {
-        await updateRecipe(rec.id);
+        await updateRecipe(rec.id, !rec.studentFriendly);
         await showRecipes();
       });
     parent.appendChild(clone);
   });
 }
 showRecipes();
+
+function handleSubmit() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", async (e) => {
+    console.log(e);
+    //stop refresh
+    e.preventDefault();
+    const formData = new FormData(form);
+    console.log(formData.get("ingredients").split("\n"));
+
+    await createRecipe({
+      name: formData.get("name"),
+      description: formData.get("description"),
+      ingredients: formData.get("ingredients").split("\n"),
+      serves: formData.get("serves"),
+      allergens: formData.get("allergens").split("\n"),
+      diet: formData.get("diet"),
+      studentFriendly: formData.get("studentFriendly"),
+      origin: formData.get("origin"),
+    });
+    showRecipes();
+  });
+}
+handleSubmit();
