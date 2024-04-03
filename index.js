ff26b2b3fd55157790e58149d93f294a21717297
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2024-04-03 21:14:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,20 +16,20 @@ samle data FormData()
     .getAll()
 */
 async function showRecipes() {
-  const response = await getRecipes();
-  console.table(response);
-  const el = document.querySelector("template").content;
-  const parent = document.querySelector(".recipes");
-  parent.innerHTML = "";
-  response.forEach((rec) => {
-    const clone = el.cloneNode(true);
-    clone.querySelector("[data-name]").textContent = rec.name;
-    clone.querySelector("[data-origin]").textContent = rec.origin;
-    if (rec.studentFriendly) {
-      clone.querySelector(".status").hidden = false;
-    } else {
-      clone.querySelector(".status").hidden = true;
-    }
+    const response = await getRecipes();
+    console.table(response);
+    const el = document.querySelector("template").content;
+    const parent = document.querySelector(".recipes");
+    parent.innerHTML = "";
+    response.forEach((rec) => {
+        const clone = el.cloneNode(true);
+        clone.querySelector("[data-name]").textContent = rec.name;
+        clone.querySelector("[data-origin]").textContent = rec.origin;
+        if (rec.studentFriendly) {
+        clone.querySelector(".status").hidden = false;
+        } else {
+        clone.querySelector(".status").hidden = true;
+        }
 
     clone.querySelectorAll("[data-id]").forEach((e) => (e.dataset.id = rec.id));
     clone
