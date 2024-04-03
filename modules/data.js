1cb345c0975a25725c6812047e539a44217a2c0b
Jonas Holbech <jh@jonasholbech.dk>
2024-04-03 09:40:38 +0200

    update, delete

--- modules/data.js
+++ modules/data.js
@@ -45,3 +45,40 @@ export async function createRecipe() {
   let data = await response.text();
   console.log(data);
 }
+
+export async function deleteRecipe(id) {
+  let headersList = {
+    Accept: "application/json",
+    apikey: apikey,
+    Prefer: "return=representation",
+  };
+
+  let response = await fetch(endpoint + "?id=eq." + id, {
+    method: "DELETE",
+    headers: headersList,
+  });
+
+  let data = await response.json();
+  return data;
+}
+export async function updateRecipe(id) {
+  let headersList = {
+    Accept: "application/json",
+    apikey: apikey,
+    Prefer: "return=representation",
+    "Content-Type": "application/json",
+  };
+
+  let bodyContent = JSON.stringify({
+    studentFriendly: false,
+  });
+
+  let response = await fetch(endpoint + "?id=eq." + id, {
+    method: "PATCH",
+    body: bodyContent,
+    headers: headersList,
+  });
+
+  let data = await response.json();
+  return data;
+}
